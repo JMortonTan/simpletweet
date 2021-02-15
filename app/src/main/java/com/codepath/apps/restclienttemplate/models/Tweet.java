1a0cb5bbe1c81940995d9f762e7dc5de4997e303
JMortonTan <jmtan188@gmail.com>
2021-02-14 23:50:56 -0500

    Part 2 Checkpoint 4. Persistant tweets from the past.

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,5 +1,13 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.provider.ContactsContract;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import com.codepath.apps.restclienttemplate.ParseRelativeDate;
 
 import org.json.JSONArray;
@@ -11,10 +19,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Parcel
+@Entity(foreignKeys = @ForeignKey(entity=User.class, parentColumns = "id", childColumns = "userId"))
 public class Tweet {
-    public String body;
-    public String createdAt;
+
+    @PrimaryKey
+    @ColumnInfo
     public long id;
+
+    @ColumnInfo
+    public String body;
+
+    @ColumnInfo
+    public String createdAt;
+
+    @ColumnInfo
+    public Long userId;
+
+    @Ignore
     public User user;
 
     // empty constructor needed by Parceler Library
@@ -25,8 +46,9 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = ParseRelativeDate.getRelativeTimeAgo(jsonObject.getString("created_at"));
         tweet.id = jsonObject.getLong("id");
-        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
-
+        User user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.user = user;
+        tweet.userId = user.id;
         return tweet;
     }
 
